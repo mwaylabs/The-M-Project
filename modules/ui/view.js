de3972c11c509f0b1ede336bda7a86a43db53454
Dominik <d.laubach@mwaysolutions.com>
2010-10-26 13:43:35 +0200

    Initial MProject skeleton

---
+++ modules/ui/view.js
@@ -0,0 +1,8 @@
+// ==========================================================================
+// Project:   MProject - Mobile HTML5 Application Framework
+// Copyright: ©2010 M-Way Solutions GmbH. All rights reserved.
+// Creator:   Dominik
+// Date:      26.10.2010
+// License:   ...
+// ==========================================================================
+
