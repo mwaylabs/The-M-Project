25afda0edeaac4b8f44e231a1d85045ef128bd94
Alexander <a.henka@mwaysolutions.com>
2010-10-27 14:01:03 +0200

    Added comment for extend function

--- modules/core/object.js
+++ modules/core/object.js
@@ -29,6 +29,9 @@ M.Object = {
     },
 
     /**
+     * Creates a new class and extends it with all functions of the defined super class
+     * The function takes multi input arguments. Each argument Servers as additional
+     * super classes - see mixins.
      * 
      */
     extend: function(){
