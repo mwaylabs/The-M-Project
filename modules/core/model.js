5eab5dba628da1c826eb4455ae5014a8c213d318
Sebastian <s.werler@mwaysolutions.com>
2010-10-28 11:36:57 +0200

    basic structure of model

---
+++ modules/core/model.js
@@ -0,0 +1,37 @@
+// ==========================================================================
+// Project:   The M-Project - Mobile HTML5 Application Framework
+// Copyright: ©2010 M-Way Solutions GmbH. All rights reserved.
+// Creator:   basti
+// Date:      28.10.2010
+// License:   Dual licensed under the MIT or GPL Version 2 licenses.
+//            http://github.com/mwaylabs/The-M-Project/blob/master/MIT-LICENSE
+//            http://github.com/mwaylabs/The-M-Project/blob/master/GPL-LICENSE
+// ==========================================================================
+
+
+m_require('logger.js');
+
+/**
+ * @class
+ *
+ * The root class for every model.
+ *
+ */
+M.Model = M.Object.extend({
+
+    /**
+     *
+     * globally unique identifier
+     *
+     * @property {String}
+    */
+    primaryKey: 'guid',
+
+
+    find: function(query){
+        
+    }
+
+    
+
+});
