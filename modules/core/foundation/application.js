0b796b38bc334c3a8e393b80c3d75b76cafb3130
Sebastian <s.werler@mwaysolutions.com>
2010-11-02 16:24:51 +0100

    Added application object for encapsulation

---
+++ modules/core/foundation/application.js
@@ -0,0 +1,21 @@
+// ==========================================================================
+// Project:   The M-Project - Mobile HTML5 Application Framework
+// Copyright: ©2010 M-Way Solutions GmbH. All rights reserved.
+// Creator:   Sebastian
+// Date:      02.11.2010
+// License:   Dual licensed under the MIT or GPL Version 2 licenses.
+//            http://github.com/mwaylabs/The-M-Project/blob/master/MIT-LICENSE
+//            http://github.com/mwaylabs/The-M-Project/blob/master/GPL-LICENSE
+// ==========================================================================
+
+m_require('view_manager.js');
+
+/**
+ * @class
+ *
+ * The root class for an application.
+ *
+ */
+M.Application = M.Object.extend({
+    viewManager: M.ViewManager
+});
