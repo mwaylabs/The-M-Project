b5a4fc4de9307259885d9890393b26aa7299168d
Dominik <d.laubach@mwaysolutions.com>
2010-11-02 13:32:56 +0100

    Added M.ViewManager, a singleton that contains all information about the application's views.

---
+++ modules/core/foundation/view_manager.js
@@ -0,0 +1,51 @@
+// ==========================================================================
+// Project:   The M-Project - Mobile HTML5 Application Framework
+// Copyright: ©2010 M-Way Solutions GmbH. All rights reserved.
+// Creator:   Dominik
+// Date:      02.11.2010
+// License:   Dual licensed under the MIT or GPL Version 2 licenses.
+//            http://github.com/mwaylabs/The-M-Project/blob/master/MIT-LICENSE
+//            http://github.com/mwaylabs/The-M-Project/blob/master/GPL-LICENSE
+// ==========================================================================
+
+m_require('view.js');
+
+M.ViewManager = M.Object.extend({
+
+    nextId: 0,
+
+    idPrefix: 'm_',
+
+    viewList: [],
+
+    getNextId: function() {
+        this.nextId = this.nextId + 1;
+        return this.idPrefix + this.nextId;
+    },
+
+    register: function(view) {
+        this.viewList.push({
+            id: view.id,
+            view: view
+        });
+    },
+
+    getViewById: function(id) {
+        for (i in this.viewList) {
+            if(this.viewList[i].id === id) {
+                return this.viewList[i].view;
+            }
+        }
+        return null;
+    },
+
+    getIdByView: function(view) {
+        for (i in this.viewList) {
+            if(this.viewList[i].view === view) {
+                return this.viewList[i].id;
+            }
+        }
+        return null;
+    }
+
+});
