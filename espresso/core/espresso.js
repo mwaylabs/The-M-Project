298e30ff6918368178cae126050c37bb053b64d7
Alexander <a.henka@mwaysolutions.com>
2010-11-02 14:07:36 +0100

    changed some folder names

---
+++ espresso/core/espresso.js
@@ -0,0 +1,24 @@
+// ==========================================================================
+// Project:   The M-Project - Mobile HTML5 Application Framework
+// Copyright: ©2010 M-Way Solutions GmbH. All rights reserved.
+// Creator:   alexander
+// Date:      29.10.2010
+// License:   Dual licensed under the MIT or GPL Version 2 licenses.
+//            http://github.com/mwaylabs/The-M-Project/blob/master/MIT-LICENSE
+//            http://github.com/mwaylabs/The-M-Project/blob/master/GPL-LICENSE
+// ==========================================================================
+
+/**
+* Definition for all espresso related functions.
+* Each file using espresso or parts of it, should include espresso.js via 'require('./espresso');'
+*
+*/
+var
+  self = this;
+
+  /*
+   * including the server.
+   */
+  self.Server = require('./server').Server;
+  self.App = require('./m_app').App;
+
---
+++ espresso/core/server.js
@@ -0,0 +1,69 @@
+// ==========================================================================
+// Project:   The M-Project - Mobile HTML5 Application Framework
+// Copyright: ©2010 M-Way Solutions GmbH. All rights reserved.
+// Creator:   alexander
+// Date:      29.10.2010
+// License:   Dual licensed under the MIT or GPL Version 2 licenses.
+//            http://github.com/mwaylabs/The-M-Project/blob/master/MIT-LICENSE
+//            http://github.com/mwaylabs/The-M-Project/blob/master/GPL-LICENSE
+// ==========================================================================
+
+
+var self = this,
+    _l, Server;
+    _l = {};
+ 
+
+_l.sys = require('sys');
+_l.http = require('http');
+_l.url = require('url');
+
+
+self.Server = function() {
+
+  this.port = 8000;
+  this.hostname = '127.0.0.1';
+
+};
+
+Server = self.Server;
+
+
+Server.prototype.run = function(app) {
+
+    var that = this;
+
+    var appName = app.name;
+    var appTheme = app.theme;
+    var appLanguage = app.buildLanguage;
+
+    _l.http.createServer(function (req, res) {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+
+        var reqURL = _l.url.parse(req.url);
+
+
+
+        _l.sys.puts(reqURL.pathname);
+
+        if(reqURL.pathname == '/'+appName){
+
+        res.write('Espresso Server Responding to: '+ appName);
+        res.write('<\/br>');
+        res.write('using theme: '+ appTheme);
+        res.write('<\/br>');
+        res.write('using language: ' +appLanguage);
+
+        }else{
+          res.write(reqURL.pathname + ' not found !');
+        }
+        res.end();
+
+       
+    }).listen(that.port, that.hostname);
+
+    _l.sys.puts('Server running at http://127.0.0.1:'+that.port+'/'+appName);
+
+
+}
+
