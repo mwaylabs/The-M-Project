89402dc5b89c44d2016c7344391138350154aeba
Dominik <d.laubach@mwaysolutions.com>
2010-11-03 10:45:57 +0100

    Added basic M.ListView

---
+++ modules/ui/list.js
@@ -0,0 +1,31 @@
+// ==========================================================================
+// Project:   The M-Project - Mobile HTML5 Application Framework
+// Copyright: ©2010 M-Way Solutions GmbH. All rights reserved.
+// Creator:   Dominik
+// Date:      03.11.2010
+// License:   Dual licensed under the MIT or GPL Version 2 licenses.
+//            http://github.com/mwaylabs/The-M-Project/blob/master/MIT-LICENSE
+//            http://github.com/mwaylabs/The-M-Project/blob/master/GPL-LICENSE
+// ==========================================================================
+
+m_require('../core/foundation/view.js');
+
+/**
+ * @class
+ *
+ * The root object for ListViews.
+ *
+ */
+M.ListView = M.View.extend({
+
+    type: 'M.ListView',
+
+    render: function() {
+
+    },
+
+    renderUpdate: function() {
+        
+    }
+
+});
