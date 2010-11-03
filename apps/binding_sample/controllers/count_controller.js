562e23213fa05ebbe6c407a9720c8472d23d2dfa
Dominik <d.laubach@mwaysolutions.com>
2010-11-03 09:05:29 +0100

    Added a sample binding app

---
+++ apps/binding_sample/controllers/count_controller.js
@@ -0,0 +1,19 @@
+// ==========================================================================
+// Project:   The M-Project - Mobile HTML5 Application Framework
+// Copyright: ©2010 M-Way Solutions GmbH. All rights reserved.
+// Creator:   Dominik
+// Date:      03.11.2010
+// License:   Dual licensed under the MIT or GPL Version 2 licenses.
+//            http://github.com/mwaylabs/The-M-Project/blob/master/MIT-LICENSE
+//            http://github.com/mwaylabs/The-M-Project/blob/master/GPL-LICENSE
+// ==========================================================================
+
+BindingSample.CountController = M.Controller.extend({
+
+    numberOfClicks: 0,
+
+    addClick: function() {
+        this.set('numberOfClicks', this.numberOfClicks + 1);
+    }
+
+});
