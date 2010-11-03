16fe89dcac38a50ba9f0037bd542603c2990d77d
Dominik <d.laubach@mwaysolutions.com>
2010-11-03 10:44:52 +0100

    Added some basic stuff for our twitter-search-app

--- apps/demo/demo.js
+++ apps/demo/demo.js
@@ -10,8 +10,9 @@
 
 var Demo = Demo || {};
 
-document.write("<script src='views/login_view.js'></script>");
 document.write("<script src='controllers/login_controller.js'></script>");
 document.write("<script src='fixtures/user_model.js'></script>");
 document.write("<script src='data_provider/user_model_data_provider.js'></script>");
 document.write("<script src='models/user_model.js'></script>");
+document.write("<script src='views/twitter_item.js'></script>");
+document.write("<script src='controllers/twitter_controller.js'></script>");
