eebaab4da96e68ed7bdf7a977a83b80cde40b833
Dominik <d.laubach@mwaysolutions.com>
2010-11-03 10:46:07 +0100

    Added basic M.ListItemView

---
+++ modules/ui/list_item.js
@@ -0,0 +1,31 @@
+// ==========================================================================
+// Project:   The M-Project - Mobile HTML5 Application Framework
+// Copyright: ©2010 M-Way Solutions GmbH. All rights reserved.
+// Creator:   Dominik
+// Date:      03.11.2010
+// License:   Dual licensed under the MIT or GPL Version 2 licenses.
+//            http://github.com/mwaylabs/The-M-Project/blob/master/MIT-LICENSE
+//            http://github.com/mwaylabs/The-M-Project/blob/master/GPL-LICENSE
+// ==========================================================================
+
+m_require('../core/foundation/view.js');
+
+/**
+ * @class
+ *
+ * The root object for ListItemViews.
+ *
+ */
+M.ListItemView = M.View.extend({
+
+    type: 'M.ListItemView',
+
+    render: function() {
+
+    },
+
+    renderUpdate: function() {
+
+    }
+
+});
