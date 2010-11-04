ab4c0a5ad09b98d40a13ea24bd300be8cc678d28
Sebastian <s.werler@mwaysolutions.com>
2010-11-04 13:59:09 +0100

    model manager to manage models inside a controller

---
+++ modules/core/foundation/model_manager.js
@@ -0,0 +1,45 @@
+// ==========================================================================
+// Project:   The M-Project - Mobile HTML5 Application Framework
+// Copyright: ©2010 M-Way Solutions GmbH. All rights reserved.
+// Creator:   Sebastian
+// Date:      04.11.2010
+// License:   Dual licensed under the MIT or GPL Version 2 licenses.
+//            http://github.com/mwaylabs/The-M-Project/blob/master/MIT-LICENSE
+//            http://github.com/mwaylabs/The-M-Project/blob/master/GPL-LICENSE
+// ==========================================================================
+
+m_require('model.js');
+
+M.ModelManager = M.Object.extend({
+
+    modelList: [],
+
+    nextId: 0,
+
+    getNextId: function() {
+        this.nextId = this.nextId + 1;
+        return this.nextId;
+    },
+
+    add: function(obj) {
+        obj.id = this.getNextId();
+        this.modelList.push(obj);
+    },
+
+    remove: function(modelId) {
+        obj = this.getModelForId(modelId);
+        if(obj) {
+            this.modelList = _.select(this.modelList, function(m){
+                return m.id !== obj.id;
+            });
+        }
+    },
+
+    getModelForId: function(id) {
+        var model = _.detect(this.modelList, function(m){
+            return m.id === id;
+        });
+        return model;
+    }
+
+});
