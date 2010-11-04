80774a0ec110bde02da84ed3c82abc9bb8fa715a
Sebastian <s.werler@mwaysolutions.com>
2010-11-04 13:58:27 +0100

    todos - a simple sample app

---
+++ apps/todos/controllers/todo_controller.js
@@ -0,0 +1,38 @@
+// ==========================================================================
+// Project:   The M-Project - Mobile HTML5 Application Framework
+// Copyright: ©2010 M-Way Solutions GmbH. All rights reserved.
+// Creator:   Sebastian
+// Date:      04.11.2010
+// License:   Dual licensed under the MIT or GPL Version 2 licenses.
+//            http://github.com/mwaylabs/The-M-Project/blob/master/MIT-LICENSE
+//            http://github.com/mwaylabs/The-M-Project/blob/master/GPL-LICENSE
+// ==========================================================================
+
+Todos.TodoController = M.Controller.extend({
+
+    notes: M.ModelManager.extend({}),
+
+    todos: null,
+
+    counter: 0,
+
+    addTodo: function() {
+        var text = '';
+        var note = Todos.Note.create( { text: text } );
+        this.notes.add(note);
+        this.set('todos', this.notes.modelList);
+        this.calculateCounter();
+    },
+
+    removeTodo: function(modelId) {
+        this.notes.remove(modelId);
+        this.set('todos', this.notes.modelList);
+        this.calculateCounter();
+    },
+
+    calculateCounter: function() {
+        this.set('counter', this.todos.length);
+    }
+
+
+});
