f4339887804aa97c2570bf00135f55db80b9a978
Sebastian <s.werler@mwaysolutions.com>
2010-11-05 11:06:06 +0100

    added sample app that demonstrates bidirectional binding

---
+++ apps/dualbind/controllers/input_controller.js
@@ -0,0 +1,19 @@
+// ==========================================================================
+// Project:   The M-Project - Mobile HTML5 Application Framework
+// Copyright: (c) 2010 M-Way Solutions GmbH. All rights reserved.
+// Creator:   Sebastian
+// Date:      05.11.2010
+// License:   Dual licensed under the MIT or GPL Version 2 licenses.
+//            http://github.com/mwaylabs/The-M-Project/blob/master/MIT-LICENSE
+//            http://github.com/mwaylabs/The-M-Project/blob/master/GPL-LICENSE
+// ==========================================================================
+
+DualBind.InputController = M.Controller.extend({
+
+    input: '',
+
+    updateInput: function(newInput) {
+        this.set('input', newInput);
+    }
+
+});
