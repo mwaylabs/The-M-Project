4efba880e15444883fa2b25491a124f62559a682
Dominik <d.laubach@mwaysolutions.com>
2010-11-09 11:16:11 +0100

    M.EventDispatcher now is part of M.Application, to have a central access point to all core components

--- modules/core/foundation/application.js
+++ modules/core/foundation/application.js
@@ -40,14 +40,30 @@ M.Application = M.Object.extend({
     modelRegistry: M.ModelRegistry,
 
     /**
+     * The application's event dispatcher.
+     *
+     * @property {Object}
+     */
+    eventDispatcher: M.EventDispatcher,
+
+    /**
      * The application's main-method, that is called automatically on load of the app.
      * Inside this method the rendering is initiated and all pages are bound to the 'pageshow'
      * event so one can do some action whenever a page is loaded.
      */
     main: function() {
+        var that = this;
+
+        /* live is jQuery fn that binds an event to all elements matching a certain selector now and in the future */
+        var eventList = 'click change keyup focus blur';
+        $('*[id]').live(eventList, function(evt) {
+            that.eventDispatcher.eventDidHappen(evt);
+        });
+
         for(i in this.viewManager.viewList) {
             if(this.viewManager.viewList[i].type === 'M.PageView') {
                 this.viewManager.viewList[i].render();
+                /* bind the pageshow event to any view's pageDidLoad property function */
                 $('#' + this.viewManager.viewList[i].id).bind('pageshow', this.bindToCaller(this.viewManager.viewList[i], this.viewManager.viewList[i].pageDidLoad));
                 $('#' + this.viewManager.viewList[i].id).page();
             }
