391995dba563f797cd36b9a3da41a6a5eb1b4a08
Dominik <d.laubach@mwaysolutions.com>
2010-11-10 14:18:16 +0100

    Added lots of comments

--- modules/ui/list.js
+++ modules/ui/list.js
@@ -50,34 +50,61 @@ M.ListView = M.View.extend({
     },
 
     renderUpdate: function() {
+
+        /* Remove all list items if the removeItemsOnUpdate property is set to YES */
         if(this.removeItemsOnUpdate) {
             this.removeAllItems();
         }
-        that = this;    
+
+        /* Save this in variable that for later use within an other scope (e.g. _each()) */
+        that = this;
+
+        /* Get the list view's content as an object from the assigned content binding */
         var content = eval(this.contentBinding);
+
+        /* Get the list view's template view for each list item */
         templateView = this.listItemTemplateView;
-        /* If there is an items property, re-assign this to content.
-           Otherwise just iterate through content itself */ 
+        
+        /* If there is an items property, re-assign this to content, otherwise iterate through content itself */ 
         if(this.items) {
             content = content[this.items];
         }
         _.each(content, function(item) {
+
+            /* Create a new object for the current template view */
             var obj = templateView.design({});
+
+            /* Get the child views as an array of strings */
             var childViewsArray = obj.childViews[0].split(' ');
+
+            /* If the item is a model, read the values from the 'record' property instead */
+            var record = item.type === 'M.Model' ? item.record : item;
+
+            /* Iterate through all views defined in the template view */
             for(var i in childViewsArray) {
+
+                /* Create a new object for the current view */
                 obj[childViewsArray[i]] = obj[childViewsArray[i]].design({});
+
+                /* Set renderToDOM to NO, since we push the HTML directly to the addItem method later on */
                 obj[childViewsArray[i]].renderToDOM = NO;
+
+                /* This regex looks for a variable inside the template view (<%= ... %>) ... */
                 var regexResult = /^<%=\s+([.|_|-|$|¤|a-zA-Z]+[0-9]*[.|_|-|$|¤|a-zA-Z]*)\s*%>$/.exec(obj[childViewsArray[i]].value);
+
+                /* ... if a match was found, the variable is replaced by the corresponding value inside the record */
                 if(regexResult) {
                     switch (obj[childViewsArray[i]].type) {
                         case 'M.LabelView':
                         case 'M.ButtonView':
                         case 'M.ImageView':
-                            obj[childViewsArray[i]].value = item[regexResult[1]];
+                            obj[childViewsArray[i]].value = record[regexResult[1]];
                             break;
                     }
                 }
             }
+
+            /* If edit mode is on, render a delete button */
             if(that.inEditMode) {
                 obj.inEditMode = that.inEditMode;
                 obj.deleteButton = obj.deleteButton.design({
@@ -86,11 +113,17 @@ M.ListView = M.View.extend({
                     action: that.editOptions.action
                 });
             }
+
+            /* Add the current list view item to the list view ... */
             that.addItem(obj.render());
+
+            /* ... once it is in the DOM, make it look nice */
             for(var i in childViewsArray) {
                 obj[childViewsArray[i]].applyTheme();
             }
         });
+
+        /* Finally let the whole list look nice */
         this.applyTheme();
     },
 
@@ -102,7 +135,10 @@ M.ListView = M.View.extend({
     },
 
     /**
+     * This method activates the edit mode and forces the list view to re-render itself
+     * and to display a remove button for every list view item.
      *
+     * @param {Object} options The options for the remove button.
      */
     toggleRemove: function(options) {
         if(eval(this.contentBinding)) {
