19940fe9372f509395f9e5c45d1ded113f5e485f
Dominik <d.laubach@mwaysolutions.com>
2010-11-11 14:48:18 +0100

    Implemented M.Cypher that is called from the 'outside world' to process some internal decoding, encoding or hashing stuff

--- modules/core/utility/cypher.js
+++ modules/core/utility/cypher.js
@@ -8,7 +8,8 @@
 //            http://github.com/mwaylabs/The-M-Project/blob/master/GPL-LICENSE
 // ==========================================================================
 
-m_require('../foundation/request.js');
+m_require('cypher_algorithms/base64.js');
+m_require('cypher_algorithms/sha256.js');
 
 /**
  * @class
@@ -25,16 +26,106 @@ M.Cypher = M.Object.extend({
      */
     type: 'M.Cypher',
 
-    decode: function() {
+    defaultDecoder: M.Base64,
+
+    defaultEncoder: M.Base64,
+
+    defaultHasher: M.SHA256,
+
+    decode: function(input, algorithm) {
+
+        if(algorithm && algorithm.decode) {
+            return algorithm.decode(input);
+        } else {
+            return this.defaultDecoder.decode(input);
+        }
         
     },
 
-    encode: function() {
+    encode: function(input, algorithm) {
+
+        if(algorithm && algorithm.encode) {
+            return algorithm.encode(input);
+        } else {
+            return this.defaultEncoder.encode(input);
+        }
 
     },
 
-    hash: function() {
-        
+    hash: function(input, algorithm) {
+
+        if(algorithm && algorithm.hash) {
+            return algorithm.hash(input);
+        } else {
+            return this.defaultHasher.hash(input);
+        }
+
+    },
+
+    /**
+     * Private method for UTF-8 encoding
+     *
+     * @private
+     * @param {String} string The string to be encoded.
+     */
+    utf8_encode : function (string) {
+        string = string.replace(/\r\n/g, '\n');
+        var utf8String = '';
+
+        for (var n = 0; n < string.length; n++) {
+
+            var c = string.charCodeAt(n);
+
+            if (c < 128) {
+                utf8String += String.fromCharCode(c);
+            }
+            else if((c > 127) && (c < 2048)) {
+                utf8String += String.fromCharCode((c >> 6) | 192);
+                utf8String += String.fromCharCode((c & 63) | 128);
+            }
+            else {
+                utf8String += String.fromCharCode((c >> 12) | 224);
+                utf8String += String.fromCharCode(((c >> 6) & 63) | 128);
+                utf8String += String.fromCharCode((c & 63) | 128);
+            }
+
+        }
+
+        return utf8String;
+    },
+
+    /**
+     * Private method for UTF-8 decoding
+     *
+     * @private
+     * @param {String} string The string to be decoded.
+     */
+    utf8_decode : function (utf8String) {
+        var string = '';
+        var i = 0;
+        var c = c1 = c2 = 0;
+
+        while ( i < utf8String.length ) {
+
+            c = utf8String.charCodeAt(i);
+
+            if (c < 128) {
+                string += String.fromCharCode(c);
+                i++;
+            }  else if((c > 191) && (c < 224)) {
+                c2 = utf8String.charCodeAt(i+1);
+                string += String.fromCharCode(((c & 31) << 6) | (c2 & 63));
+                i += 2;
+            } else {
+                c2 = utf8String.charCodeAt(i+1);
+                c3 = utf8String.charCodeAt(i+2);
+                string += String.fromCharCode(((c & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63));
+                i += 3;
+            }
+
+        }
+
+        return string;
     }
 
 });
