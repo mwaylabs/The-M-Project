5025b4618525568e073f2c81bce2363e0f06db8c
Dominik <d.laubach@mwaysolutions.com>
2010-11-11 16:47:39 +0100

    M.Application now registeres an orientationChange event on startup

--- modules/core/foundation/application.js
+++ modules/core/foundation/application.js
@@ -62,8 +62,13 @@ M.Application = M.Object.extend({
         var that = this;
 
         /* live is jQuery fn that binds an event to all elements matching a certain selector now and in the future */
-        var eventList = 'click change keyup focus blur';
+        var eventList = 'click change keyup focus blur orientationchange';
         $('*[id]').live(eventList, function(evt) {
+            that.eventDispatcher.eventDidHappen(evt);
+        });
+
+        /* also bind the orientationchange event to the body of the application */
+        $('body').bind('orientationchange', function(evt) {
             that.eventDispatcher.eventDidHappen(evt);
         });
 
@@ -73,6 +78,11 @@ M.Application = M.Object.extend({
                 /* bind the pageshow event to any view's pageDidLoad property function */
                 $('#' + this.viewManager.viewList[i].id).bind('pageshow', this.bindToCaller(this.viewManager.viewList[i], this.viewManager.viewList[i].pageDidLoad));
                 $('#' + this.viewManager.viewList[i].id).page();
+
+                /* set the first page as current page to be displayed */
+                if(!this.viewManager.getCurrentPage()) {
+                    this.viewManager.setCurrentPage(this.viewManager.viewList[i]);
+                }
             }
         }
     }
