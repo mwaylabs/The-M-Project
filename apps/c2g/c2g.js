bafd68fb9f934510ea2028fc57ef7a64468528ca
Dominik <d.laubach@mwaysolutions.com>
2010-11-11 10:10:58 +0100

    Added skeleton for c2g app

---
+++ apps/c2g/c2g.js
@@ -0,0 +1,15 @@
+// ==========================================================================
+// Project:   The-M-Project - Mobile HTML5 Application Framework
+// Copyright: ©2010 M-Way Solutions GmbH. All rights reserved.
+// Creator:   Sebastian
+// Date:      04.11.2010
+// License:   ...
+// ==========================================================================
+
+// todos.js simulates the whole sourcecode of the app.
+
+var C2G = C2G || {};
+
+//document.write("<script src='views/todo_item.js'></script>");
+
+
