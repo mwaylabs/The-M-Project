3db16799d64ef5267fed565330e15d1bfd5572b7
Sebastian <s.werler@mwaysolutions.com>
2010-11-16 16:56:15 +0100

    cleaned.

--- modules/core/datastore/data_provider.js
+++ modules/core/datastore/data_provider.js
@@ -14,7 +14,8 @@ m_require('logger.js');
  * available data source types
  */
 M.FIXTURE = 'fixture';
-M.WEBSTORAGE = 'webstorage';
+M.LOCALSTORAGE = 'localStorage';
+M.WEBSQL = 'webSql';
 M.REMOTE = 'remote';
 
 /**
@@ -34,76 +35,35 @@ M.DataProvider = M.Object.extend({
     type: 'M.DataProvider',
 
     /**
-     * defines the data source type
-     *
-     * @property {String}
-     */
-    useSource: '',
-
-    /**
-     * defines the parameters for data source: fixture
-     *
-     * @property {Object}
-     */
-    configureFixture: null,
-
-    /**
-     * defines the parameters for data source: web storage
-     *
-     * @property {Object}
-     */
-    configureWebStorage: null,
-
-    /**
-     * defines the parameters for data source: web service
-     *
-     * @property {Object}
-     */
-    configureRemote: null,
-
-    /**
-     * Delegates the query to the source, defined in the concrete implementation of DataProvider.
+     * Interface method.
+     * Implemented by specific data provider.
      *
      * @param {String} query The query string.
      */
     find: function(query) {
-        var data = null;
-        switch (this.useSource) {
-            case M.FIXTURE:
-                data = this.getDataFromFixture(query);
-                break;
-            case M.REMOTE:
-                this.getDataFromRemote(query);
-                break;
-        }
-        return data;
     },
 
     /**
-     * Fetches the data from a specified fixture, based on the passed query.
-     *
-     * @param {String} query The query string.
+     * 
      */
-    getDataFromFixture: function(query) {
-        /* 'source' identifies the model's fixture */
-        return this.configureFixture.source;
+    save: function() {
     },
 
     /**
-     * Fetches the data from a remote server (e.g. webservice), based on the passed query.
      *
-     * @param {String} query The query string.
      */
-    getDataFromRemote: function(query) {
-        var that = this;
-        M.Request.init({
-            url: this.configureRemote.get.url,
-            type: this.configureRemote.get.type,
-            isJSON: this.configureRemote.get.isJSON,
-            onSuccess: function(data, msg, request) {
-                that.bindToCaller(scope, callback)(data);
-            }
-        }).send();
+    del: function() {
+
+    },
+
+    /**
+     * Checks if object has certain property.
+     *
+     * @param {obj} obj The object to check.
+     * @param {String} prop The property to check for.
+     */
+    check: function(obj, prop) {
+       return obj[prop] ? YES : NO;
     }
 
 });
