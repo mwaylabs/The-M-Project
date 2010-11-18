04823ba8ddc69861ffab2dc2c088b213aabd4553
Dominik <d.laubach@mwaysolutions.com>
2010-11-18 11:36:03 +0100

    Added resources to tabbar-sample

---
+++ apps/todos2/controllers/todo_controller.js
@@ -0,0 +1,65 @@
+// ==========================================================================
+// Project:   The M-Project - Mobile HTML5 Application Framework
+// Copyright: ©2010 M-Way Solutions GmbH. All rights reserved.
+// Creator:   Sebastian
+// Date:      04.11.2010
+// License:   Dual licensed under the MIT or GPL Version 2 licenses.
+//            http://github.com/mwaylabs/The-M-Project/blob/master/MIT-LICENSE
+//            http://github.com/mwaylabs/The-M-Project/blob/master/GPL-LICENSE
+// ==========================================================================
+
+Todos.TodoController = M.Controller.extend({
+
+    notes: M.ModelManager.extend({
+        model: Todos.Note
+    }),
+
+    todos: null,
+
+    counter: 0,
+
+    init: function() {
+        this.notes.find();
+        this.set('todos', this.notes.modelList);
+        this.calculateCounter();
+    },
+
+    addTodo: function() {
+        var text = Todos.app.page.content.inputField.value;
+        if(!text) {
+            return;
+        }
+
+        var note = Todos.Note.newRecord( { text: text } );
+        this.notes.add(note);
+        note.save();
+        this.set('todos', this.notes.modelList);
+
+        this.calculateCounter();
+
+        Todos.app.page.content.inputField.setValue('');
+    },
+
+    removeTodo: function(domId, modelId) {
+        var doDelete = confirm('Do you really want to delete this item?');
+        if(doDelete) {
+            this.notes.del(modelId);
+            this.notes.remove(modelId);
+            this.set('todos', this.notes.modelList);
+            this.calculateCounter();
+        }
+    },
+
+    calculateCounter: function() {
+        this.set('counter', this.todos.length);
+    },
+
+    edit: function() {
+        Todos.app.page.content.todoList.toggleRemove({
+            target: this,
+            action: 'removeTodo',
+            disableOnEdit: ['Todos.app.page.content.inputField']
+        });
+    }
+
+});
---
+++ apps/todos2/todos.js
@@ -0,0 +1,16 @@
+// ==========================================================================
+// Project:   The-M-Project - Mobile HTML5 Application Framework
+// Copyright: ©2010 M-Way Solutions GmbH. All rights reserved.
+// Creator:   Sebastian
+// Date:      04.11.2010
+// License:   ...
+// ==========================================================================
+
+// todos.js simulates the whole sourcecode of the app.
+
+var Todos = Todos || {};
+
+document.write("<script src='models/note.js'></script>");
+document.write("<script src='controllers/todo_controller.js'></script>");
+document.write("<script src='views/todo_item.js'></script>");
+document.write("<script src='views/tabs.js'></script>");
