ec91fdfa851e7c5f876526d5666a597ce62d9f32
Dominik <d.laubach@mwaysolutions.com>
2010-11-24 17:15:05 +0100

    Refactoring: Added getView(), getPage() and findView() for easier access to views out of the app

--- modules/core/foundation/view_manager.js
+++ modules/core/foundation/view_manager.js
@@ -66,6 +66,13 @@ M.ViewManager = M.Object.extend({
     currentDialog: null,
 
     /**
+     * A reference to the latest found view which is necassary for the findView() method.
+     *
+     * @property {Object}
+     */
+    foundView: null,
+
+    /**
      * Returns the next Id build from nextId property incremented by 1 and the prefix.
      * The id is used as the value for the HTML attribute id.
      */
@@ -84,7 +91,7 @@ M.ViewManager = M.Object.extend({
     },
 
     /**
-     * Returns the view object from the viewlist array identified
+     * Returns the view object from the view list array identified
      * by the value of its id attribute.
      *
      * @param {String} id
@@ -103,6 +110,75 @@ M.ViewManager = M.Object.extend({
      */
     getIdByView: function(view) {
         return view.id;
+    },
+
+    /**
+     * Returns the view object from the view list array identified by the view's
+     * name and its page. If there are multiple views with the same name on the
+     * same page, the first result is returned.
+     *
+     * Note: Try to use unique names for your views within the same page!
+     *
+     * @param {String, Object} parentView. The name of the parent view or the parent view itself.
+     * @param {String} targetView. The name of the view to be returned.
+     */
+    getView: function(parentView, targetView) {
+        if(typeof(parentView) !== 'object') {
+            parentView = M.Application.pages['page1'];  
+        }
+        var view = null;
+
+        /* reset previously found views before searching again */
+        this.foundView = null;
+        if(parentView) {
+            view = this.findView(parentView, targetView);
+        }
+
+        if(!view) {
+            M.Logger.log('view \'' + targetView + '\' not found on page \'' + targetView + '\'', M.WARN);
+        }
+        return view;
+    },
+
+    /**
+     * Searches for a certain view within a given parent view. If it is found, the result
+     * is returned. Otherwise the search algorithm checks for possible child views and then
+     * recursively searches within these child views for the target view.
+     *
+     * This method is mainly used by the getView() method to find a view within a page.
+     *
+     * @param {Object} parentView. The parent view to search in.
+     * @param {String} targetView. The name of the view to be returned.
+     */
+    findView: function(parentView, targetView) {
+        if(parentView.childViews) {
+            var childViews = $.trim(parentView.childViews).split(' ');
+            for(var i in childViews) {
+                if(targetView === childViews[i]) {
+                    this.foundView =  parentView[targetView];
+                } else {
+                    this.findView(parentView[childViews[i]], targetView);
+                }
+            }
+        }
+        return this.foundView;
+    },
+
+    /**
+     * Returns the page object from the view list array identified by its name. If
+     * there are multiple pages with the same name, the first result is returned.
+     *
+     * Note: Try to use unique names for your pages!
+     *
+     * @param {String} pageName. The name of the page to be returned.
+     */
+    getPage: function(pageName) {
+        var page = M.Application.pages[pageName];
+
+        if(!page) {
+            M.Logger.log('page \'' + pageName + '\' not found', M.WARN);
+        }
+        return page;
     },
 
     /**
