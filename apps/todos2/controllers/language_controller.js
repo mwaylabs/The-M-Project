7c164615f80ec4f729d27abd78730c33c1a6df80
Dominik <d.laubach@mwaysolutions.com>
2010-11-30 16:35:52 +0100

    Added third tab bar item 'settings' for language-changing (not yet implemented)

--- apps/todos2/controllers/language_controller.js
+++ apps/todos2/controllers/language_controller.js
@@ -1,10 +1,31 @@
 Todos.LanguageController = M.Controller.extend({
 
-   changeLanguage: function() {
+    languages: null,
+
+    init: function() {
+
+        M.Request.init({
+            url: 'langs.json',
+            isJSON: YES,
+            beforeSend: function(req) {
+                //...
+            },
+            onSuccess: function(data){
+                Todos.LanguageController.set('languages', data.languages);
+            }
+        }).send();
+
+    },
+
+    success: function(data) {
+
+    },
+
+    changeLanguage: function() {
 
        var language = M.ViewManager.getView('page3', 'langSelection').getSelection();
        console.log(language);
 
-   }
+    }
 
 });
