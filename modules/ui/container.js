aa89d159aced75a194b7843bf1401baf18965592
Dominik <d.laubach@mwaysolutions.com>
2010-12-01 11:04:46 +0100

    Added M.ContainerView, a simple div-container that can e.g. be used for third party JS libraries like raphael

---
+++ modules/ui/container.js
@@ -0,0 +1,45 @@
+// ==========================================================================
+// Project:   The M-Project - Mobile HTML5 Application Framework
+// Copyright: (c) 2010 M-Way Solutions GmbH. All rights reserved.
+// Creator:   Dominik
+// Date:      01.12.2010
+// License:   Dual licensed under the MIT or GPL Version 2 licenses.
+//            http://github.com/mwaylabs/The-M-Project/blob/master/MIT-LICENSE
+//            http://github.com/mwaylabs/The-M-Project/blob/master/GPL-LICENSE
+// ==========================================================================
+
+/**
+ * @class
+ *
+ * The root object for ContainerView.
+ *
+ */
+M.ContainerView = M.View.extend({
+
+    /**
+     * The type of this object.
+     *
+     * @property {String}
+     */
+    type: 'M.ContainerView',
+
+    /**
+     * Renders a simple div container and applies css classes if specified.
+     */
+    render: function() {
+        this.html += '<div id="' + this.id + '"' + this.style() + '></div>';      
+        return this.html;
+    },
+
+    /**
+     * Applies some style-attributes to the container view.
+     */
+    style: function() {
+        var html = '';
+        if(this.cssClass) {
+            html += ' class="' + this.cssClass + '"';
+        }
+        return html;
+    }
+
+});
