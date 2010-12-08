5ef4956f34ae2d8d5157e11081fbd3c8498533fc
Dominik <d.laubach@mwaysolutions.com>
2010-12-08 14:08:49 +0100

    Added JSDoc conform comments and did some refactoring, e.g. removed unused properties

--- modules/core/foundation/view.js
+++ modules/core/foundation/view.js
@@ -13,7 +13,9 @@ m_require('core/foundation/model.js');
 /**
  * @class
  *
- * The root of all views.
+ * M.View defines the prototype for any view within The M-Project. It implements lots of basic
+ * properties and methods that are used in many derived views. M.View specifies a default
+ * behaviour for functionalities like rendering, theming, delegating updates etc.
  *
  * @extends M.Object
  */
@@ -28,41 +30,34 @@ M.View = M.Object.extend(
     type: 'M.View',
 
     /**
-     * A boolean value to definitely recognize a view as a view.
+     * A boolean value to definitely recognize a view as a view, independent on its
+     * concrete type, e.g. M.ButtonView or M.LabelView.
      *
      * @type Boolean
      */
     isView: YES,
 
     /**
-     * Generic attribute for views. Some subclasses have a property that is more readable to its context than 'value'.
-     * Internally these properties are mapped to 'value'.
-     * e.g. in LabelView: value:this.text
+     * The value property is a generic property for all values. Even if not all views
+     * really use it, e.g. the wrapper views like M.ButtonGroupView, most of it do.
      *
-     * @property {Object, String}
+     * @property {String}
      */
     value: null,
 
     /**
      * The path to a content that is bind to the view's value.
      *
-     * @property {String, Object}
+     * @property {String}
      */
     contentBinding: null,
 
     /**
      * An array specifying the view's children.
      *
-     * @type Object
+     * @type Array
      */
     childViews: null,
-
-    /**
-     * Determines whether this view renders directly to the DOM or just returns its HTML representation.
-     *
-     * @type Boolean
-     */
-    renderToDOM: YES,
 
     /**
      * Indicates whether this view currently has the focus or not.
@@ -72,23 +67,19 @@ M.View = M.Object.extend(
     hasFocus: NO,
 
     /**
-     * The id of the View used for the HTML attribute ID.
+     * The id of the view used for the html attribute id. Every view gets its own unique
+     * id during the rendering process.
      */
     id: null,
 
     /**
-     * Indicates whether the view should be displayed inline or not.
+     * Indicates whether the view should be displayed inline or not. This property isn't
+     * supported by all views, but e.g. by M.LabelView or M.ButtonView.
      */
     isInline: NO,
 
-    /**
-     * Indicates whether the view is visible or not.
-     * Implemented via CSS's display property.
-     */
-    isVisible: YES,
-
     /*
-     * Indicates whether the view is enabled or not.
+     * Indicates whether the view is currently enabled or disabled.
      */
     isEnabled: YES,
 
@@ -99,18 +90,23 @@ M.View = M.Object.extend(
      */
     parentView: null,
 
+    /**
+     * If a view represents a model, e.g. within a list view, this property is used to save
+     * the model's id. So the view can be used to get to the record.
+     *
+     * @param {Object}
+     */
     modelId: null,
 
     /**
-     * This property can be used to assign a css class to the view using the jquery mobile
-     * specific data-theme property. This allows you to create your custom styles.
+     * This property can be used to assign a css class to the view to get a custom styling.
      *
      * @type String
      */
     cssClass: null,
 
     /**
-     * This property can be used to assign a css style to the view using. This allows you to
+     * This property can be used to assign a css style to the view. This allows you to
      * create your custom styles inline.
      *
      * @type String
@@ -120,7 +116,7 @@ M.View = M.Object.extend(
     /**
      * This property can be used to assign a css class to the view if an error occurs. The
      * applying of this class is automatically triggered if the validation of the view
-     * goes wrong.
+     * goes wrong. This property is mainly used by input views, e.g. M.TextFieldView.
      *
      * @type String
      */
@@ -130,7 +126,7 @@ M.View = M.Object.extend(
      * This property can be used to assign a css class to the view on its initialization. This
      * property is mainly used for input ui elements like text fields, that might have a initial
      * value that should be rendered in a different style than the later value entered by the
-     * user.
+     * user. This property is mainly used by input views, e.g. M.TextFieldView.
      *
      * @type String
      */
@@ -141,7 +137,7 @@ M.View = M.Object.extend(
      * It is once set within the render method and then eventually updated within the
      * renderUpdate method.
      *
-     * @type String The pages html content.
+     * @type String
      */
     html: '',
 
@@ -192,9 +188,11 @@ M.View = M.Object.extend(
     },
 
      /**
-     * Interface method.
-     * Renders itself to HTML.
-     * Render implementation is done by the special child obj of View (LabelView etc.).
+     * This is the basic render method for any views. It does not specific rendering, it just calls
+     * renderChildViews method. Most views overwrite this method with a custom render behaviour.
+     * 
+     * @private
+     * @returns {String} The list item view's html representation.
      */
     render: function() {
         this.renderChildViews();
@@ -202,16 +200,20 @@ M.View = M.Object.extend(
     },
 
     /**
-     * Interface method.
-     * Updates itself in DOM.
-     * Render implementation is done by the special child obj of View (LabelView etc.).
+     * @interface
+     *
+     * This method defines an interface method for updating an already rendered html representation
+     * of a view. This should be implemented with a specific behaviour for any view.
      */
     renderUpdate: function() {
 
     },
 
     /**
-     * Triggers render() on all children.
+     * Triggers render() on all children. This method defines a basic behaviour for rendering a view's
+     * child views. If a custom behaviour for a view is desired, the view has to overwrite this method.
+     *
+     * @private
      */
     renderChildViews: function() {
         if(this.childViews) {
@@ -253,15 +255,16 @@ M.View = M.Object.extend(
     },
 
     /**
-     * Interface method.
-     * Triggers rendering engine to style this view/apply a theme.
+     * This method is a basic implementation for theming a view. It simply calls the
+     * themeChildViews method. Most views overwrite this method with a custom theming
+     * behaviour.
      */
     theme: function() {
         this.themeChildViews();
     },
 
     /**
-     * Triggers theme() on all children.
+     * This method triggers the theme method on all children.
      */
     themeChildViews: function() {
         if(this.childViews) {
@@ -273,8 +276,9 @@ M.View = M.Object.extend(
     },
 
     /**
-     * contentDidChange is called by the observable when observable's state did change.
-     * It updates the view's value property.
+     * The contentDidChange method is automatically called by the observable when the
+     * observable's state did change. It then updates the view's value property based
+     * on the specified content binding.
      */
     contentDidChange: function(){
         var bindingPath = this.contentBinding ? this.contentBinding.split('.') : this.computedValue.contentBinding.split('.');
@@ -294,7 +298,8 @@ M.View = M.Object.extend(
     },
 
     /**
-     * This method attaches itself to an observable.
+     * This method attaches the view to an observable to be later notified once the observable's
+     * state did change.
      *
      * @param {String} contentBinding The path to the observable property.
      */
@@ -309,8 +314,10 @@ M.View = M.Object.extend(
     },
 
     /**
-     * Interface method.
-     * This method sets its value to the value it has in its DOM representation.
+     * @interface
+     * 
+     * This method defines an interface method for setting the view's value from its DOM
+     * representation. This should be implemented with a specific behaviour for any view.
      */
     setValueFromDOM: function() {
 
@@ -331,42 +338,57 @@ M.View = M.Object.extend(
     },
 
     /**
-     * Interface method.
-     * Method to append css styles and other style specific inline attributes to the rendered view.
+     * @interface
+     *
+     * This method defines an interface method for styling the view. This should be
+     * implemented with a specific behaviour for any view.
      */
     style: function() {
 
     },
 
     /**
-     * This method is called whenever the view gets the focus.
+     * This method is called whenever the view got the focus and basically only sets
+     * the view's hasFocus property to YES. If a more complex behaviour is desired,
+     * a view has to overwrite this method.
      */
     gotFocus: function() {
         this.hasFocus = YES;
     },
 
     /**
-     * This method is called whenever the view lost the focus.
+     * This method is called whenever the view lost the focus and basically only sets
+     * the view's hasFocus property to NO. If a more complex behaviour is desired,
+     * a view has to overwrite this method.
      */
     lostFocus: function() {
         this.hasFocus = NO;
     },
 
     /**
-     * Secure the passed string.
-     * 
-     * This is mainly used for securing input elements like text fields but since it is
-     * part of M.View it can be used and called out of any view.
+     * This method secure the passed string. It is mainly used for securing input elements
+     * like M.TextFieldView but since it is part of M.View it can be used and called out
+     * of any view.
      *
      * So far we only replace '<' and '>' with their corresponding html entity. The functionality
-     * of this method will be extended in the future.
+     * of this method will be extended in the future. If a more complex behaviour is desired,
+     * any view using this method has to overwrite it.
      *
-     * @param {String} str The string to be escaped
+     * @param {String} str The string to be secured.
+     * @returns {String} The secured string.
      */
     secure: function(str) {
         return str.replace(/</g, "&lt;").replace(/>/g, "&gt;");
     },
 
+    /**
+     * This method parses a given string, replaces any new line, '\n', with a line break, '<br/>',
+     * and returns the modified string. This can be useful especially for input views, e.g. it is
+     * used in context with the M.TextFieldView.
+     *
+     * @param {String} str The string to be modified.
+     * @returns {String} The modified string.
+     */
     nl2br: function(str) {
         if(str) {
             if(typeof(str) !== 'string') {
@@ -378,16 +400,9 @@ M.View = M.Object.extend(
     },
 
     /**
-     * This method writes the view's html string into the DOM.
-     */
-    writeToDOM: function() {
-        document.write(this.html);
-    },
-
-    /**
      * Adds a css class to the view's DOM representation.
      *
-     * @type String cssClass The css class to be added.
+     * @param {String} cssClass The css class to be added.
      */
     addCssClass: function(cssClass) {
         $('#' + this.id).addClass(cssClass);
@@ -396,7 +411,7 @@ M.View = M.Object.extend(
     /**
      * Removes a css class to the view's DOM representation.
      *
-     * @type String cssClass The css class to be removed.
+     * @param {String} cssClass The css class to be added.
      */
     removeCssClass: function(cssClass) {
         $('#' + this.id).removeClass(cssClass);
@@ -405,18 +420,17 @@ M.View = M.Object.extend(
     /**
      * Adds or updates a css property to the view's DOM representation.
      *
-     * @type String key The property's name.
-     * @type String value The property's value.
+     * @param {String} key The property's name.
+     * @param {String} value The property's value.
      */
     setCssProperty: function(key, value) {
         $('#' + this.id).css(key, value);
     },
 
     /**
-     * Adds or updates a css property to the view's DOM representation.
+     * Removes a css property from the view's DOM representation.
      *
-     * @type String key The property's name.
-     * @type String value The property's value.
+     * @param {String} key The property's name.
      */
     removeCssProperty: function(key) {
         this.setCssProperty(key, '');
