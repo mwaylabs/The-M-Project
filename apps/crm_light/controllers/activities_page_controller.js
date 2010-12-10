9b4ccd6d3ff9aef60ac44a3077383bb3c34ec548
Dominik <d.laubach@mwaysolutions.com>
2010-12-10 12:03:46 +0100

    Updated controllers

--- apps/crm_light/controllers/activities_page_controller.js
+++ apps/crm_light/controllers/activities_page_controller.js
@@ -1,5 +1,26 @@
 CRMLight.ActivitiesPageController = M.Controller.extend({
 
+    activities: null,
+
+    init: function() {
+
+        M.Request.init({
+            url: 'activities.json',
+            isJSON: YES,
+            beforeSend: function(req) {
+                M.LoaderView.show();
+            },
+            onSuccess: function(data){
+                M.LoaderView.hide();
+                CRMLight.ActivitiesPageController.set('activities', data);
+            },
+            onError: function(request, message){
+                M.LoaderView.hide();
+            }
+        }).send();
+
+    },
+
     openStartPage: function() {
 
         this.switchToPage(M.ViewManager.getPage('startPage'), null, YES);
