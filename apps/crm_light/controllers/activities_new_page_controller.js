baa1d5e07af49aeb19242ecc1174aa30e02f6a5b
Dominik <d.laubach@mwaysolutions.com>
2010-12-13 16:40:17 +0100

    updated app

--- apps/crm_light/controllers/activities_new_page_controller.js
+++ apps/crm_light/controllers/activities_new_page_controller.js
@@ -1,5 +1,26 @@
 CRMLight.ActivitiesNewPageController = M.Controller.extend({
 
+    activity: null,
+
+    activityName: null,
+
+    activityStatus: null,
+
+    activityReason: null,
+
+    activityResult: null,
+
+    init: function(isFirstTime) {
+
+        if(isFirstTime) {
+            this.set('activityName', this.activity.name);
+            this.set('activityStatus', this.activity.values['status']);
+            this.set('activityReason', this.activity.values['activity_reason']);
+            this.set('activityResult', this.activity.values['result']);
+        }
+
+    },
+
     openActivitiesPage: function() {
 
         this.switchToPage(M.ViewManager.getPage('activitiesPage'), null, YES);
