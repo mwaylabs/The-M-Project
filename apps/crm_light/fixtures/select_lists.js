1a438035de4cc3ae2249a2ff32465005faa488ef
Sebastian <s.werler@mwaysolutions.com>
2010-12-13 14:45:56 +0100

    data for select lists

---
+++ apps/crm_light/fixtures/select_lists.js
@@ -0,0 +1,39 @@
+// ==========================================================================
+// Project:   The M-Project - Mobile HTML5 Application Framework
+// Copyright: (c) 2010 M-Way Solutions GmbH. All rights reserved.
+// Creator:   Sebastian
+// Date:      13.12.2010
+// License:   Dual licensed under the MIT or GPL Version 2 licenses.
+//            http://github.com/mwaylabs/The-M-Project/blob/master/MIT-LICENSE
+//            http://github.com/mwaylabs/The-M-Project/blob/master/GPL-LICENSE
+// ==========================================================================
+
+CRMLight.SelectListFixtures = {
+    'Akquise': {
+        'Status': ['Offen', 'In Bearbeitung', 'Erledigt', 'Abgesagt'],
+        'Aktivitätsgrund': ['Akquise Termin absprechen', 'Akquise Bedarf analysieren', 'Akquise Angebot erstellen', 'Akquise Angebot abgeben', 'Akquise Neuer Objekt-Mietvertrag'],
+        'Ergebnis': ['Ziel erreicht', 'Ziel nicht erreicht', 'Folgetermin', 'Wiedervorlage', 'Wettbewerb', 'Termin nicht stattgefunden']
+    },
+
+    'Beratung': {
+        'Status': ['Offen', 'In Bearbeitung', 'Erledigt', 'Abgesagt'],
+        'Aktivitätsgrund': ['Beratung Consulting', 'Beratung Arbeitsabläufe Kunde', 'Beratung Standortanalyse', 'Beratung Produkte', 'Beratung LVP',
+                            'Beratung Promotion', 'Beratung Platzierung', 'Beratung Shopoptimierung', 'Beratung Konzepte', 'Beratung Schulung Kunde',
+                            'Beratung BWL', 'Beratung Store Check'],
+        'Ergebnis': ['Ziel erreicht', 'Ziel nicht erreicht', 'Folgetermin', 'Wiedervorlage', 'Wettbewerb', 'Termin nicht stattgefunden']
+    },
+
+    'Debitoren': {
+        'Status': ['Offen', 'In Bearbeitung', 'Erledigt', 'Abgesagt'],
+        'Aktivitätsgrund': ['OP Gespräch', 'OP Kassieren', 'OP Teilzahlungsvereinbarung', 'OP Amortisations- und Servicepauschalen', 'OP Miete und Pacht'],
+        'Ergebnis': ['Ziel erreicht', 'Ziel nicht erreicht', 'Folgetermin', 'Wiedervorlage', 'Wettbewerb', 'Termin nicht stattgefunden']
+    },
+
+    'Equipment': {
+        'Status': ['Offen', 'In Bearbeitung', 'Erledigt', 'Abgesagt'],
+        'Aktivitätsgrund': ['Equipment Backofen', 'Equipment MDE-Gerät', 'Equipment Kassensystem', 'Equipment Shopeinrichtung/Warenträger', 'Equipment Eis-? und Lagertruhe',
+                            'Equipment Kühlschrank/Kühlgerät', 'Equipment Kaffeemaschine', 'Equipment ELO-?Terminal', 'Equipment Werbeträger', 'Equipment Werbemittel'],
+        'Ergebnis': ['Ziel erreicht', 'Ziel nicht erreicht', 'Folgetermin', 'Wiedervorlage', 'Wettbewerb', 'Termin nicht stattgefunden']
+    }
+    
+};
