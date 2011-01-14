bd34491a0280837bc12790f9b09eaf7818d5ca8e
Sebastian <s.werler@mwaysolutions.com>
2011-01-14 14:57:06 +0100

    changed m_require

--- modules/core/datastore/web_sql.js
+++ modules/core/datastore/web_sql.js
@@ -8,7 +8,7 @@
 //            http://github.com/mwaylabs/The-M-Project/blob/master/GPL-LICENSE
 // ==========================================================================
 
-m_require('core/detox/data_provider.js');
+m_require('core/datastore/data_provider.js');
 
 /**
  * @class
