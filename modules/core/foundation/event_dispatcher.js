d9d64200cb128929292a610479e2bdbd446ea40e
Dominik <d.laubach@mwaysolutions.com>
2011-01-17 11:08:27 +0100

    Added wokraround for jqm's fixed header/footer bug

--- modules/core/foundation/event_dispatcher.js
+++ modules/core/foundation/event_dispatcher.js
@@ -41,6 +41,10 @@ M.EventDispatcher = M.Object.create(
      * @param {Object} evt The event.
      */
     eventDidHappen: function(evt) {
+        /* WORKAROUND FOR FOOTER / HEADER BUG IN JQM */
+        /* TODO: REMOVE ONCE IT IS FIXED BY JQM */
+        $.fixedToolbars.show();
+
         this.delegateEvent(evt.type, evt.currentTarget.id, evt.keyCode);
     },
 
