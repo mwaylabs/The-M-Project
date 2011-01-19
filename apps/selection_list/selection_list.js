21c61aa078b6f0828806486e07beae81c02e71e2
Dominik <d.laubach@mwaysolutions.com>
2011-01-19 11:02:46 +0100

    deleted apps folder

--- apps/selection_list/selection_list.js
+++
@@ -1,9 +0,0 @@
-var SelectionList = SelectionList || {};
-
-document.write("<script src='controllers/single_selection.js'></script>");
-document.write("<script src='controllers/multiple_selection.js'></script>");
-document.write("<script src='controllers/single_selection_dialog.js'></script>");
-document.write("<script src='views/tab_bar.js'></script>");
-document.write("<script src='views/single_selection.js'></script>");
-document.write("<script src='views/multiple_selection.js'></script>");
-document.write("<script src='views/single_selection_dialog.js'></script>");
