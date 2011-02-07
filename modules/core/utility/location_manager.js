a4d90e46dc170a14a7fef1f97913e7646703fc8a
Dominik <mail@dominik-laubach.de>
2011-02-07 17:09:26 +0100

    fixed path to M.Location in m_require

--- modules/core/utility/location_manager.js
+++ modules/core/utility/location_manager.js
@@ -8,7 +8,7 @@
 //            http://github.com/mwaylabs/The-M-Project/blob/master/GPL-LICENSE
 // ==========================================================================
 
-m_require('core/foundation/utility/location.js');
+m_require('core/utility/location.js');
 
 /**
  * A constant value for permisson denied error.
