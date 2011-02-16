51ebdd889c2a35af0d9a77c6844ab409d946ffd9
Dominik <mail@dominik-laubach.de>
2011-02-16 12:00:09 +0100

    added fix for browsers without html5 history api

--- modules/core/foundation/application.js
+++ modules/core/foundation/application.js
@@ -168,7 +168,9 @@ M.Application = M.Object.extend(
     showEntryPage: function() {
         var entryPage = M.ViewManager.getPage(M.Application.entryPage);
         document.location.hash = '#' + entryPage.id;
-        window.history.pushState(null, 'entryPage', 'index.html#' + entryPage.id);
+        if(window.history && typeof(window.history.pushState) === 'function') {
+            window.history.pushState(null, 'entryPage', 'index.html#' + entryPage.id);
+        }
         M.ViewManager.setCurrentPage(M.ViewManager.getPage(M.Application.entryPage));
         $.mobile.initializePage();
     }
