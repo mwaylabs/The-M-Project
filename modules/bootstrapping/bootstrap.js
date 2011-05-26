23a8d6429fecebad7af82e76189d295af3e27eed
Dominik <mail@dominik-laubach.de>
2011-05-26 12:10:31 +0200

    updated M.LoaderView to work with latest JQM version, but also added own rendering

--- modules/bootstrapping/bootstrap.js
+++ modules/bootstrapping/bootstrap.js
@@ -2,6 +2,9 @@ $(document).bind("mobileinit", function(){
     /* do something on mobile init or configure jqm */
     document.location.hash = '';
     //history.pushState(null, null, '#m_entryPage');
+
+    /* disable default loader of jqm */
+    $.mobile.loadingMessage = '';
 });
 $(document).ready(function(){
     /* do something on dom ready */
