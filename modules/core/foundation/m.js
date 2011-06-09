e4dfa077171e9393f4a429374b5e3881f037edcb
Dominik <mail@dominik-laubach.de>
2011-06-09 11:28:59 +0200

    changed copyright sign to (C)

--- modules/core/foundation/m.js
+++ modules/core/foundation/m.js
@@ -1,6 +1,6 @@
 // ==========================================================================
 // Project:   The M-Project - Mobile HTML5 Application Framework
-// Copyright: ©2010 M-Way Solutions GmbH. All rights reserved.
+// Copyright: (c) 2010 M-Way Solutions GmbH. All rights reserved.
 // Creator:   Dominik
 // Date:      26.10.2010
 // License:   Dual licensed under the MIT or GPL Version 2 licenses.
