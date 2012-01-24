ad47587076b397904bd0d9cc8f3163a7746852a9
dominiklaubach <d.laubach@panacoda.com>
2012-01-24 16:47:19 +0100

    Content binding now can handle 0 as Number (#29)

--- modules/core/foundation/view.js
+++ modules/core/foundation/view.js
@@ -472,7 +472,7 @@ M.View = M.Object.extend(
             }
         });
 
-        if(!value && value !== '') {
+        if(value === undefined || value === null) {
             M.Logger.log('The value assigned by content binding (property: \'' + contentBinding.property + '\') for ' + this.type + ' (' + (this._name ? this._name + ', ' : '') + '#' + this.id + ') is invalid!', M.WARN);
             return;
         }
