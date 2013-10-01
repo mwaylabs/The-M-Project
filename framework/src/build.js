3be0fea6d4374c8e9fbdb0c9ec2f8a085c877d79
stefanbuck <web@broud.de>
2013-10-01 17:58:02 +0200

    add amd support

--- framework/src/build.js
+++ framework/src/build.js
@@ -1,48 +1,65 @@
-var M = (function( global, Backbone, _ ) {
+(function (window, factory) {
 
-    /**
-     * Defines the general namespace
-     *
-     * @type {Object}
-     */
-    var M = {};
+    // AMD
+    if (typeof define === 'function' && define.amd) {
+        return define(['backbone', 'underscore', 'jquery', 'backboneLayoutManager', 'stickIt', 'tmpl'], factory);
+    }
 
-    // @include ./core/m.js
-    // @include ./core/const.js
-    // @include ./core/object.js
-    // @include ./core/logger.js
-    // @include ./core/view_manager.js
-    // @include ./core/application.js
+    // Browser globals.
+    window.M = factory.call(window, window.Backbone, window._, window.$);
 
-    // @include ./utility/i18n.js
-    // @include ./utility/objectid.js
-    // @include ./utility/uuid.js
 
-    // @include ./connection/request.js
-    // @include ./connection/request_manager.js
-    // @include ./connection/socket_io.js
-    // @include ./data/field.js
-    // @include ./data/entity.js
-    // @include ./data/model.js
-    // @include ./data/collection.js
-    // @include ./data/data_selector.js
-    // @include ./data/sql_selector.js
-    // @include ./data/stores/store.js
-    // @include ./data/stores/socket.js
-    // @include ./data/stores/local_storage.js
-    // @include ./data/stores/web_sql.js
-    // @include ./data/stores/bikini_store.js
+}(typeof global === "object" ? global : this, function (Backbone, _, $) {
 
-    // @include ./ui/views/view.js
-    // @include ./ui/pagetransitions.js
-    // @include ./ui/layouts/layout.js
-    // @include ./ui/layout_manager.js
-    // @include ./ui/views/button.js
+    var M = (function( global, Backbone, _, $ ) {
 
-    // @include ./ui/layouts/header-layout/header-layout.js
-    // @include ./ui/layouts/bottom-bar-layout/bottom-bar-layout.js
-    // @include ./ui/layouts/switch-layout/switch-layout.js
-    // @include ./ui/layouts/swipe-layout/swipe-layout.js
+        /**
+         * Defines the general namespace
+         *
+         * @type {Object}
+         */
+        var M = {};
 
-    return M;
-})(this, Backbone, _);
+        // @include ./core/m.js
+        // @include ./core/const.js
+        // @include ./core/object.js
+        // @include ./core/logger.js
+        // @include ./core/view_manager.js
+        // @include ./core/application.js
+
+        // @include ./utility/i18n.js
+        // @include ./utility/objectid.js
+        // @include ./utility/uuid.js
+
+        // @include ./connection/request.js
+        // @include ./connection/request_manager.js
+        // @include ./connection/socket_io.js
+        // @include ./data/field.js
+        // @include ./data/entity.js
+        // @include ./data/model.js
+        // @include ./data/collection.js
+        // @include ./data/data_selector.js
+        // @include ./data/sql_selector.js
+        // @include ./data/stores/store.js
+        // @include ./data/stores/socket.js
+        // @include ./data/stores/local_storage.js
+        // @include ./data/stores/web_sql.js
+        // @include ./data/stores/bikini_store.js
+
+        // @include ./ui/views/view.js
+        // @include ./ui/pagetransitions.js
+        // @include ./ui/layouts/layout.js
+        // @include ./ui/layout_manager.js
+        // @include ./ui/views/button.js
+
+        // @include ./ui/layouts/header-layout/header-layout.js
+        // @include ./ui/layouts/bottom-bar-layout/bottom-bar-layout.js
+        // @include ./ui/layouts/switch-layout/switch-layout.js
+        // @include ./ui/layouts/swipe-layout/swipe-layout.js
+
+        return M;
+    })(this, Backbone, _, $);
+
+  return M;
+
+}));
