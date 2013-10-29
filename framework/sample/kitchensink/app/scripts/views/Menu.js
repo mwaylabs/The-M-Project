a1b19ad8c5386eb842c030f52363902286c0b74f
hano <its@time2dev.com>
2013-10-29 07:44:03 +0100

    bugfix in sample app

--- framework/sample/kitchensink/app/scripts/views/Menu.js
+++ framework/sample/kitchensink/app/scripts/views/Menu.js
@@ -46,8 +46,21 @@ Kitchensink.Views = Kitchensink.Views || {};
                 })
             }),
 
-            buttonExample: M.ButtonView.extend({
+            localizationExample: M.ButtonView.extend({
                 value: M.I18N.get('global.switchLanguage'),
+                events: {
+                    tap: function() {
+                        if(M.I18N._activeLocale == 'de') {
+                            M.I18N.setLocale('en');
+                        } else {
+                            M.I18N.setLocale('de');
+                        }
+                    }
+                }
+            }),
+
+            pageswitchExample: M.ButtonView.extend({
+                value: M.I18N.get('global.pageswitch'),
                 events: {
                     tap: function() {
                         if(M.I18N._activeLocale == 'de') {
