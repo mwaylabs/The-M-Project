42b51071cf68e6d66f03f22d10954252fcc72d83
hano <its@time2dev.com>
2013-11-14 22:45:04 +0100

    add swipe to the tab view

--- sample/kitchensink/app/scripts/controllers/Page4Controller.js
+++ sample/kitchensink/app/scripts/controllers/Page4Controller.js
@@ -18,13 +18,19 @@
             M.Layout.prototype._render.apply(this, arguments);
         },
 
-        _postRender: function(){
+        _postRender: function() {
             M.Layout.prototype._postRender.apply(this, arguments);
             this.$el.addClass(this.scrolling ? 'scrolling' : '');
 
         },
 
         switchToTab: function( index ) {
+            if( index < 0 || index >= Object.keys(this._tabMenu.childViews).length ) {
+                return;
+            }
+            //$tablayout-menu-scroll-button-width: 200px;
+            var buttonWidth = 200;
+            this.$el.find('[data-childviews=tab-menu]').scrollLeft( index * buttonWidth - 50 );
             this._tabMenu.setActive(index);
             this.$el.find('.m-tab.m-page-current').removeClass('m-page-current');
             this.childViews['tab-content'][index].$el.addClass('m-page-current');
@@ -40,27 +46,54 @@
 
             var contents = [];
             var grid = 'col-xs-' + (12 / tabs.length);
-            if(this.scrolling){
+            if( this.scrolling ) {
                 grid = '';
             }
             for( var t = 0; t < tabs.length; t++ ) {
-                var button = M.ButtonView.extend({
-                    value: tabs[t].headline,
+                var button = this._createButton({
                     index: t,
-                    grid:  grid,
-                    events: {
-                        tap: function( event, element ) {
-                            that.switchToTab(element.index);
-                        }
-                    }
-                }).create();
+                    grid: grid,
+                    headline: tabs[t].headline
+                });
                 this._tabMenu.addChildView('button' + t, button);
-                contents.push(tabs[t].content.create());
+                contents.push(this._extendContent({
+                    index: t,
+                    content: tabs[t].content
+                }));
             }
 
             this.addChildView('tab-menu', this._tabMenu);
             this.addChildView('tab-content', contents);
+        },
+
+        _createButton: function( options ) {
+            var that = this;
+            return M.ButtonView.extend({
+                value: options.headline,
+                index: options.index,
+                grid: options.grid,
+                events: {
+                    tap: function( event, element ) {
+                        that.switchToTab(element.index);
+                    }
+                }
+            }).create();
+        },
+
+        _extendContent: function( options ) {
+            var that = this;
+            return options.content.extend({
+                events: {
+                    dragleft: function( event, element ) {
+                        that.switchToTab(options.index + 1);
+                    },
+                    dragright: function( event, element ) {
+                        that.switchToTab(options.index - 1);
+                    }
+                }
+            }).create()
         }
+
     });
 
 
@@ -110,42 +143,161 @@
                     headline: 'Tab1',
                     content: M.TabView.extend({
                         value: 'Tab1 Content'
+                    }, {
+                        c1: M.View.extend({
+                            cssClass: 'inner-content',
+                            value: 'content content',
+                            events: {
+                                drag: function( event, element ) {
+                                    element._setValue('drag did happen');
+                                    element.render();
+                                },
+                                dragleft: function( event, element ) {
+                                    element._setValue('dragleft did happen');
+                                },
+                                dragright: function( event, element ) {
+                                    element._setValue('dragright did happen');
+                                }
+                            }
+                        })
                     })
                 },
                 {
                     headline: 'Tab2',
                     content: M.TabView.extend({
                         value: 'Tab2 Content'
+                    }, {
+                        c2: M.View.extend({
+                            cssClass: 'inner-content',
+                            value: 'content content',
+                            events: {
+                                drag: function( event, element ) {
+                                    element._setValue('drag did happen');
+                                    element.render();
+                                },
+                                dragleft: function( event, element ) {
+                                    element._setValue('dragleft did happen');
+                                },
+                                dragright: function( event, element ) {
+                                    element._setValue('dragright did happen');
+                                }
+                            }
+                        })
                     })
                 },
                 {
                     headline: 'Tab3',
                     content: M.TabView.extend({
                         value: 'Tab3 Content'
+                    }, {
+                        c3: M.View.extend({
+                            cssClass: 'inner-content',
+                            value: 'content content',
+                            events: {
+                                drag: function( event, element ) {
+                                    element._setValue('drag did happen');
+                                    element.render();
+                                },
+                                dragleft: function( event, element ) {
+                                    element._setValue('dragleft did happen');
+                                },
+                                dragright: function( event, element ) {
+                                    element._setValue('dragright did happen');
+                                }
+                            }
+                        })
                     })
                 },
                 {
                     headline: 'Tab4',
                     content: M.TabView.extend({
                         value: 'Tab4 Content'
+                    }, {
+                        c4: M.View.extend({
+                            cssClass: 'inner-content',
+                            value: 'content content',
+                            events: {
+                                drag: function( event, element ) {
+                                    element._setValue('drag did happen');
+                                    element.render();
+                                },
+                                dragleft: function( event, element ) {
+                                    element._setValue('dragleft did happen');
+                                },
+                                dragright: function( event, element ) {
+                                    element._setValue('dragright did happen');
+                                }
+                            }
+                        })
                     })
                 },
                 {
                     headline: 'Tab5',
                     content: M.TabView.extend({
                         value: 'Tab5 Content'
+                    }, {
+                        c5: M.View.extend({
+                            cssClass: 'inner-content',
+                            value: 'content content',
+                            events: {
+                                drag: function( event, element ) {
+                                    element._setValue('drag did happen');
+                                    element.render();
+                                },
+                                dragleft: function( event, element ) {
+                                    element._setValue('dragleft did happen');
+                                },
+                                dragright: function( event, element ) {
+                                    element._setValue('dragright did happen');
+                                }
+                            }
+                        })
                     })
                 },
                 {
                     headline: 'Tab6',
                     content: M.TabView.extend({
                         value: 'Tab6 Content'
+                    }, {
+                        c6: M.View.extend({
+                            cssClass: 'inner-content',
+                            value: 'content content',
+                            events: {
+                                drag: function( event, element ) {
+                                    element._setValue('drag did happen');
+                                    element.render();
+                                },
+                                dragleft: function( event, element ) {
+                                    element._setValue('dragleft did happen');
+                                },
+                                dragright: function( event, element ) {
+                                    element._setValue('dragright did happen');
+                                }
+                            }
+                        })
                     })
                 },
                 {
                     headline: 'Tab7',
                     content: M.TabView.extend({
                         value: 'Tab7 Content'
+                    }, {
+                        c7: M.View.extend({
+                            cssClass: 'inner-content',
+                            value: 'content content',
+                            events: {
+                                drag: function( event, element ) {
+                                    element._setValue('drag did happen');
+                                    element.render();
+                                },
+                                dragleft: function( event, element ) {
+                                    element._setValue('dragleft did happen');
+                                },
+                                dragright: function( event, element ) {
+                                    element._setValue('dragright did happen');
+                                }
+                            }
+                        })
                     })
                 }
 
