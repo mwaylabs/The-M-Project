49343b43f64fd10a04a931513fc5702aaac86db4
stefanbuck <web@broud.de>
2013-11-14 15:17:40 +0100

    delete unused files

--- test.old/connection/request_manager_test.js
+++
@@ -1,7 +0,0 @@
-test('M.RequestManager Test', function() {
-
-    ok(M.hasOwnProperty('RequestManager'), 'M.RequestManager is defined.');
-
-    ok(M.RequestManager.getObjectType() === 'M.RequestManager', 'The type of M.RequestManager is M.RequestManager.');
-
-});
--- test.old/example_test.js
+++
@@ -1,4 +0,0 @@
-module("example_test");
-test( "example test", function() {
-	ok( 1 == "1", "Passed!" );
-});
