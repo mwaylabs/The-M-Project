8d73ca514bffa6c7ffcea54eccbcf9c7b6e69855
hano <its@time2dev.com>
2013-11-28 11:45:44 +0100

    add listitem constants

--- src/ui/views/listitem.js
+++ src/ui/views/listitem.js
@@ -5,12 +5,44 @@
  * @extends M.View
  */
 M.ListItemView = M.View.extend({
+
+    /**
+     * The type of the view
+     * @type String
+     * @private
+     */
     _type: 'M.ListItemView',
-    template: _.tmpl(M.TemplateManager.get('M.ListItemView')),
-    initialize: function () {
+
+    /**
+     * The template of the view
+     *
+     */
+    _template: _.tmpl(M.TemplateManager.get('M.ListItemView')),
+
+    /**
+     * The type of the listitem. Default is 'basic' and just displays the value
+     * @type {String} select from the M.ListItemView.CONS
+     * @example
+     *
+     * M.ListItemView.extend({
+     *
+     *   type: M.ListItemView.CONS.LINKED
+     *
+     * });
+     *
+     */
+    type: 'basic',
+
+    initialize: function() {
         M.View.prototype.initialize.apply(this, arguments);
-        if (this.templateExtend) {
+        if( this.templateExtend ) {
             this.template = _.tmpl(this.template({value: this.templateExtend}));
         }
     }
 }).implements([M.ActiveState]);
+
+
+M.ListItemView.CONS = {
+    LINKED: 'linked',
+    BASIC: 'basic'
+}
