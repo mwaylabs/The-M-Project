ea8d55ae0a839b2ca8d76496f86e5eb631dd70cf
hano <its@time2dev.com>
2013-11-29 15:21:30 +0100

    little improvements

--- sample/addressbook/app/scripts/views/List.js
+++ sample/addressbook/app/scripts/views/List.js
@@ -20,9 +20,9 @@ Addressbook.Views = Addressbook.Views || {};
 
                 listItemView: M.ListItemView.extend({
 
-                    cssClass: 'row',
+                    cssClass: '',
 
-                    extendTemplate: '<div><div class="col-xs-8 col-xs-offset-1"><div class="name ellipsis"><span class="firstname"><%= firstname %></span><span class="lastname"><%= lastname %></span></div><div class="ellipsis"><span class="street"><%= street %></span><span class="houseno"><%= houseno %></span></div><div class="ellipsis"><span class="zip"><%= zip %></span><span class="city"><%= city %></span></div></div><div class="col-xs-2"><div class="company-logo <%= company %>"></div></div></div>',
+                    extendTemplate: '<div class="row"><div class="col-xs-7 col-xs-offset-1"><div class="name ellipsis"><span class="firstname"><%= firstname %></span><span class="lastname"><%= lastname %></span></div><div class="ellipsis"><span class="street"><%= street %></span><span class="houseno"><%= houseno %></span></div><div class="ellipsis"><span class="zip"><%= zip %></span><span class="city"><%= city %></span></div></div><div class="col-xs-3"><div class="company-logo <%= company %>"></div></div></div>',
 
                     useElement: YES,
 
@@ -52,61 +52,63 @@ Addressbook.Views = Addressbook.Views || {};
                     }
                 })
             })
-        }),
-
-        tutorial: M.View.extend({
-
-            cssClass: 'tutorial',
-
-            events: {
-                tap: 'hideTutorial'
-            }
-        }, {
-
-            welcome: M.View.extend({
-                tagName: 'h2',
-                value: M.I18N.get('global.welcome')
-            }),
-
-            intro: M.View.extend({
-                tagName: 'h3',
-                value: M.I18N.get('global.intro')
-            }),
-
-            link: M.View.extend({
-                value: '',
-                extendTemplate: '<a target="_blank" href="' + location.href + '">Open</a>'
-            }),
-
-            qr: M.ImageView.extend({
-                value: 'ressources/qr.png'
-            }),
-
-            first_step: M.View.extend({
-                tagName: 'h4',
-                value: M.I18N.get('global.first_step')
-            }),
-
-            second_step: M.View.extend({
-                tagName: 'h4',
-                value: M.I18N.get('global.second_step')
-            }),
-
-            third_step: M.View.extend({
-                tagName: 'h4',
-                value: M.I18N.get('global.third_step')
-            }),
-
-            thanks: M.ButtonView.extend({
-                value: M.I18N.get('global.dont_show_again'),
-                events:{
-                    tap: function(){
-                        localStorage.setItem('tutorial', NO);
-                    }
-                }
-            })
-
         })
+
+//        ,
+//
+//        tutorial: M.View.extend({
+//
+//            cssClass: 'tutorial',
+//
+//            events: {
+//                tap: 'hideTutorial'
+//            }
+//        }, {
+//
+//            welcome: M.View.extend({
+//                tagName: 'h2',
+//                value: M.I18N.get('global.welcome')
+//            }),
+//
+//            intro: M.View.extend({
+//                tagName: 'h3',
+//                value: M.I18N.get('global.intro')
+//            }),
+//
+//            link: M.View.extend({
+//                value: '',
+//                extendTemplate: '<a target="_blank" href="' + location.href + '">Open</a>'
+//            }),
+//
+//            qr: M.ImageView.extend({
+//                value: 'ressources/qr.png'
+//            }),
+//
+//            first_step: M.View.extend({
+//                tagName: 'h4',
+//                value: M.I18N.get('global.first_step')
+//            }),
+//
+//            second_step: M.View.extend({
+//                tagName: 'h4',
+//                value: M.I18N.get('global.second_step')
+//            }),
+//
+//            third_step: M.View.extend({
+//                tagName: 'h4',
+//                value: M.I18N.get('global.third_step')
+//            }),
+//
+//            thanks: M.ButtonView.extend({
+//                value: M.I18N.get('global.dont_show_again'),
+//                events:{
+//                    tap: function(){
+//                        localStorage.setItem('tutorial', NO);
+//                    }
+//                }
+//            })
+//
+//        })
     });
 
 })();
