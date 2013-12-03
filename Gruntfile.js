98eecf0d4efc0a29c59d21ae25b55eb6e6830238
Kai Stritzelberger <k.stritzelberger@mwaysolutions.com>
2013-12-03 14:12:18 +0100

    add grunt clean-task and grunt curl-task
    grunt clean for deleting md files curl task for downloading them
    automatically from github. Implemented tutorials into jsdoc

--- Gruntfile.js
+++ Gruntfile.js
@@ -157,17 +157,30 @@ module.exports = function (grunt) {
         jsdoc : {
             dist : {
                 src: ['README.md','src/connection/*.js','src/core/*.js','src/data/*.js','src/data/stores/*.js','src/interfaces/*.js','src/ui/*.js','src/ui/layouts/*.js','src/ui/views/*.js','src/utility/*.js'],
-                options: {
-                    destination: 'doc'
-                }
-            },
-            tmpl : {
-                src: ['README.md','src/connection/*.js','src/core/*.js','src/data/*.js','src/data/stores/*.js','src/interfaces/*.js','src/ui/*.js','src/ui/layouts/*.js','src/ui/views/*.js','src/utility/*.js'],
                 options:{
                     destination: 'doc',
                     template: "doc-template",
-                    configure: "doc-template/jsdoc.conf.json"
+                    configure: "doc-template/jsdoc.conf.json",
+                    tutorials: "doc-template/additional"
                 }
+            }
+        },
+        'curl-dir': {
+            customFilepaths: {
+                src: [
+                    'https://raw.github.com/mwaylabs/The-M-Project-Sample-Apps/master/README.md',
+                    'https://raw.github.com/mwaylabs/generator-tmp2/master/README.md',
+                    'https://raw.github.com/mwaylabs/The-M-Project/absinthe/thanks_to.md'
+                ],
+                router: function (url) {
+                    return url.replace('https://raw.github.com/mwaylabs/The-M-Project-Sample-Apps/master/README.md', 'sampleApps.md').replace('https://raw.github.com/mwaylabs/generator-tmp2/master/README.md', 'generator.md').replace('https://raw.github.com/mwaylabs/The-M-Project/absinthe/thanks_to.md', 'thanksTo.md');
+                },
+                dest: 'doc-template/additional'
+            }
+        },
+        clean: {
+            md: {
+                src: ["doc-template/additional/sampleApps.md", "doc-template/additional/generator.md", "doc-template/additional/thanksTo.md"]
             }
         }
     });
@@ -246,5 +259,5 @@ module.exports = function (grunt) {
     grunt.registerTask('travis', ['jsonlint', 'default', 'test']);
     grunt.registerTask('default', ['build-js', 'build-css']);
 
-    grunt.registerTask('build-doc', ['jsdoc']);
+    grunt.registerTask('build-doc', ['clean:md','curl-dir', 'jsdoc']);
 };
---
+++ doc-template/additional/thanksTo.md
@@ -0,0 +1,114 @@
+# Technologie:
+
+## html5
+<http://www.w3.org/html/logo/downloads/HTML5_Logo.svg>
+
+## javascript
+<http://seravo.fi/uploads/seravo/2013/06/JavaScript-logo-300x300.png>
+
+## css3
+<http://ohdoylerules.com/content/images/css3.svg>
+
+
+# Core:
+
+## Backbone
+<http://backbonejs.org/docs/images/backbone.png>
+
+## jQuery mobile:
+<http://jquerymobile.com/wp-content/uploads/2012/09/jquery-mobile-logo-03.png>
+
+## Topcoat:
+<http://topcoat.io/>
+
+## Bootstrap:
+<http://getbootstrap.com/>
+
+## Hammer:
+<http://eightmedia.github.io/hammer.js/img/hammer-figure.png>
+
+## stickit
+<http://nytimes.github.io/backbone.stickit/>
+
+
+# Build Tool:
+
+## Grunt:
+<http://gruntjs.com/img/grunt-logo.svg>
+
+## Bower:
+<http://bower.io/img/bower-logo.png>
+
+## Yeoman:
+<http://yeoman.io/assets/img/yeoman-logo.png>
+
+## node 
+<http://nodejs.org/images/logo.png>
+
+
+# Libraties
+
+## modernizr
+<http://modernizr.com/i/img/logo-x12.png>
+
+
+# Tools
+
+## github
+<https://github.com/logos>
+
+## webstorm
+<http://www.jetbrains.com/company/press/logos.html>
+
+# Platforms
+
+## android
+<http://developer.android.com/distribute/googleplay/promote/brand.html>
+<http://developer.android.com/images/brand/Android_Robot_200.png>
+
+
+## ios
+????
+
+## blackberry
+
+## windows phone
+
+# Company
+
+## M-Way
+<https://wiki.mwaysolutions.com/confluence/display/MWSG/M-Way+general>
+
+
+# Links
+http://backbonejs.org/
+http://underscorejs.org/
+http://jquery.com
+http://getbootstrap.com/
+http://eightmedia.github.io/hammer.js/
+http://nytimes.github.io/backbone.stickit/
+http://socket.io/
+http://modernizr.com/
+http://momentjs.com/
+https://github.com/barisaydinoglu/Detectizr
+http://fontawesome.io/
+https://github.com/alexgibson/shake.js/
+https://github.com/ftlabs/fastclick
+https://github.com/mwaylabs/grunt-tmpl
+https://github.com/mwaylabs/tmpl
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
